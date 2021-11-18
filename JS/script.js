0345107b3fa13a6de58232752d6ad709847e7ba9
AndreiZhukau <zhukau85@gmail.com>
2021-11-18 15:41:48 +0300

    lesson20

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,47 @@
-//Урок N19 - Callback - функции
+//Урок N20 - Объекты, деструктуризация объектов (ES6)
 
 'use strict';
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+      border: 'black',
+      bg: 'red'
+  },
+  makeTest: function () {
+    console.log("Test");
+  }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border);
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+//console.log(Object.keys(options));
+//console.log(Object.keys(options).length);
 
-function done() {
-    console.log('Я прошел этот урок');
-}
+//console.log(options["colors"]["border"]);
 
-learnJS('JavaScript', done);
+//delete options.name;
 
-//learnJS('JavaScript', function() {
-//    console.log('Я прошел этот урок');
-//});
+//console.log(options);
+
+//let counter = 0;
+//for (let key in options) {
+//    if (typeof(options[key]) ==='object') {
+//        for (let i in options[key]) {
+//            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//            counter++;
+//        }
+//    }  else {
+//      console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//      counter++;
+//    }
+//}
+//console.log(counter);
+
+
+
