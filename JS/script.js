186b5da55fd6acb8c6df44d71924a5cdbcadc547
AndreiZhukau <zhukau85@gmail.com>
2021-11-22 15:55:30 +0300

    lesson28

--- JS/script.js
+++ JS/script.js
@@ -1,52 +1,22 @@
-// Урок номер 27 - Задачи с собеседований на понимание основ
+// Урок номер 28 - Получение элементов со страницы
 
 'use strict';
 
-//Какое будет выведено значение:
-let x = 5; alert( x++ );
+const box = document.getElementById('box');
+console.log(box);
 
+//const btns = document.getElementsByTagName('button')[1];
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-//Чему равно такое выражение:
-[ ] + false - null + true ?  // "NuN"
+const circle = document.getElementsByClassName('circle');
+console.log(circle);
 
+const hearts = document.querySelectorAll('.heart');
+//console.log(hearts);
+hearts.forEach(item => {
+    console.log(item);
+});
 
-//Что выведет этот код:
-let y = 1; let x = y = 2; alert(x); // 2
-
-
-//Чему равна сумма
-[ ] + 1 + 2? // "12"
-
-
-//Что выведет этот код:
-alert( "1"[0] )? // 1
-
-
-//Чему равно
-2 && 1 && null && 0 && undefined ? // null; И(&&) запинается на лжи, а (||) всегда запинается на правде
-
-
-//Есть ли разница между выражениями?
-!!( a && b ) и (a && b)? // false
-
-
-//Что выведет этот код:
-alert( null || 2 && 3 || 4 ); ? // 3
-
-
-//Правда ли что a == b ?
-a = [1, 2, 3]; b = [1, 2, 3]; //false
-
-
-//Что выведет этот код:
-alert( +"Infinity" ); // Infinity
-
-
-//Верно ли сравнение:
-
-"Ёжик" > "яблоко"? //false
-
-
-//Чему равно
-
-0 || "" || 2 || undefined || true || falsе  // 2
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
