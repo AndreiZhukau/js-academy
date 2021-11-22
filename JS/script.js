facbe9ef0fffdf5264af3a94a6aa4120af879d45
AndreiZhukau <zhukau85@gmail.com>
2021-11-22 14:47:07 +0300

    lesson27

--- JS/script.js
+++ JS/script.js
@@ -1,57 +1,52 @@
-// Урок номер 26 - Динамическая типизация в JS
+// Урок номер 27 - Задачи с собеседований на понимание основ
 
 'use strict';
 
-// To String
-
-// 1)
-
-//console.log(typeof(null)));
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+//Какое будет выведено значение:
+let x = 5; alert( x++ );
 
 
-// 2)
-console.log(typeof(5 + ''));
-console.log(typeof(null + ''));
+//Чему равно такое выражение:
+[ ] + false - null + true ?  // "NuN"
 
-const num = 5;
 
-console.log("https://vk.com/catalog/" + num);
+//Что выведет этот код:
+let y = 1; let x = y = 2; alert(x); // 2
 
-const fontSize = 26 + 'px';
 
-// To Number
+//Чему равна сумма
+[ ] + 1 + 2? // "12"
 
-// 1)
-console.log(typeof(Number('4')));
 
-// 2)
-console.log(typeof(+'5'));
+//Что выведет этот код:
+alert( "1"[0] )? // 1
 
-// 3)
-console.log(typeof(parseInt("15px", 10 )));
 
-let answ = +prompt("Hello", "");
+//Чему равно
+2 && 1 && null && 0 && undefined ? // null; И(&&) запинается на лжи, а (||) всегда запинается на правде
 
-// To boolean
 
-// 0, '' null, undefined, NaN;
+//Есть ли разница между выражениями?
+!!( a && b ) и (a && b)? // false
 
-let switcher = null;
 
-if (switcher) {
-    console.log('Working...');
-}
+//Что выведет этот код:
+alert( null || 2 && 3 || 4 ); ? // 3
 
-switcher = 1;
 
-if (switcher) {
-    console.log('Working...');
-}
+//Правда ли что a == b ?
+a = [1, 2, 3]; b = [1, 2, 3]; //false
 
-// 2)
-console.log(typeof(Boolean('4')));
 
-// 3)
-console.log(typeof(!!"44444"));
+//Что выведет этот код:
+alert( +"Infinity" ); // Infinity
+
+
+//Верно ли сравнение:
+
+"Ёжик" > "яблоко"? //false
+
+
+//Чему равно
+
+0 || "" || 2 || undefined || true || falsе  // 2
