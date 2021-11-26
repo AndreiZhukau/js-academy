2a8e1ad79e02c33643a83f17543e2428e911ae71
AndreiZhukau <zhukau85@gmail.com>
2021-11-26 18:42:00 +0300

    lesson35

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,17 @@
-// Урок номер 34 - События на мобильных устройствах
+// Урок номер 35 - Async, defer, динамические скрипты
 
 'use strict';
 
-// touchstart
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// touchcancel
-
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        console.log('start');
-        //console.log(e.touches);
-        //console.log(e.targetTouches);
-        console.log(e.changedTouches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-        //console.log('move');
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    //box.addEventListener('touchend', (e) => {
-    //    e.preventDefault();
-    //    console.log('End');
-    //});
-
-});
+const p = document.querySelectorAll('p');
+console.log(p);
 
 
-// touches
-// targetTouches
-// changedTouches
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript("js/test.js");
+loadScript("js/some.js");
