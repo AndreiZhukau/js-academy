f0cc109a3176d74578705dea53ebbc9df64e7da4
AndreiZhukau <zhukau85@gmail.com>
2021-11-26 19:34:54 +0300

    lesson37

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,46 @@
-// Урок номер 35 - Async, defer, динамические скрипты
+// Урок номер 37 - ClassList и делегирование событий
 
 'use strict';
 
-const p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button');
+      wrapper = document.querySelector('.btn-block');
 
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+//console.log(btns[0].classList.length);
+//console.log(btns[0].classList.item(0));
+//console.log(btns[1].classList.add('red'));  //добавляет класс у елемента
+//console.log(btns[0].classList.remove('blue')); //удаляет класс у елемента
+//console.log(btns[0].classList.toggle('blue'));
 
-loadScript("js/test.js");
-loadScript("js/some.js");
+//if (btns[1].classList.contains('red')) {
+//    console.log('red');
+//}
+
+//btns[0].addEventListener('click', () => {
+//    if (!btns[1].classList.contains('red')) {
+//        btns[1].classList.add('red');
+//    } else {
+//        btns[1].classList.remove('red');
+//    }
+//});
+
+btns[0].addEventListener('click', () => {
+    btns[1].classList.toggle('red');
+});
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button.red")) {
+        console.log('Hello');
+    }
+});
+
+
+//btns.forEach(btn => {
+//    btn.addEventListener('click', () => {
+//        console.log('Hello');
+//    });
+//});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
