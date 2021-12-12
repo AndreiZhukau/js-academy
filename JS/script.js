16538211b7ccc005c6f609c49a913e5cc2b17897
AndreiZhukau <zhukau85@gmail.com>
2021-12-12 22:51:20 +0300

    lesson45

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,27 @@
-// Урок номер 40 - Работа с датами
+// Урок номер 45 - Функции-конструкторы
 
 'use strict';
 
-const now = new Date();
-
-console.log(now.setHours(18, 40));
-console.log(now);
-
-
-//console.log(now.getFullYear());
-//console.log(now.getMonth());
-//console.log(now.getDate());
-//console.log(now.getDay());
-//console.log(now.getUTCHours());
-
-//console.log(now.getTimezoneOffset());
-//console.log(now.getTime());
-
-let start = new Date();
-
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3;
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
 }
 
-let end = new Date();
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`);
+}
 
-alert(`цикл отработал за ${end - start} миллисекунд`);
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
